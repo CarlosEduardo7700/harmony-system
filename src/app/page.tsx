936cb1c93a74ac2f7c9926f94838007f9703a5e9
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-26 17:57:54 -0300

    feat: Definindo padrões de cores do sistema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="container-principal">
+
       <section className="header-home">
         <h1>Harmony</h1>
       </section>
+
+      <section className="container-section">
+
+      </section>
+
     </div>
   );
 }
