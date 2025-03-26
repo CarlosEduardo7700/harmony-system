d856e3b96dadf985da865e88c52fe5c2054ce52c
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-03-26 18:23:26 -0300

    feat: Criando o componente Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,6 @@
 export default function Home() {
   return (
     <div className="container-principal">
-
-      <section className="header-home">
-        <h1>Harmony</h1>
-      </section>
-
-      <section className="container-section">
-
-      </section>
-
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <header className="container-header">
+            <h1>Harmony</h1>
+        </header>
+    );
+  }
